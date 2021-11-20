01c6e79a2ad488f9c171a1f78d48d7360af4bc84
Rithik Gandhi <39555302+rithgan@users.noreply.github.com>
2021-11-20 19:29:35 +0530

    v2_address-update

--- pancake-swap-interface-v1/src/state/swap/hooks.ts
+++ pancake-swap-interface-v1/src/state/swap/hooks.ts
@@ -87,9 +87,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5Fe5cC0122403f06abE2A75DBba1860Edb762985', // v2 factory
-  '0x3E2b14680108E8C5C45C3ab5Bc04E01397af14cB', // v2 router 01
-  '0xCc7aDc94F3D80127849D2b41b6439b7CF1eB4Ae0', // v2 router 02
+  '0xf9cb726686056ac2B7d0e628A80CDf81ea652CF8', // v2 factory
+  '0x52482bE2aA6e87066d14b14cE28458BC63768313', // v2 router 01
+  '0x52482bE2aA6e87066d14b14cE28458BC63768313', // v2 router 02
 ]
 
 /**
